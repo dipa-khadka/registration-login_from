aae752d1fd88bb7f7289f2d52675940bb4fdeb76
dipa-khadka <dipshanakhadka77@gmail.com>
2023-11-09 20:13:38 +0545

    forms

--- registration/forms.py
+++ registration/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
     
-    
+    class Meta:
+        model = User
+        feilds = ('username', 'email', 'password1', 'password2')
+        
+        
+      
+     
        
         
