a1b563d87bcb3ec2baba89cbb0399826109e143e
dipa-khadka <dipshanakhadka77@gmail.com>
2023-11-09 20:19:15 +0545

    starting

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
